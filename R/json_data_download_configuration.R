1dd463a83a5ecc1cdae070f1e24837c86741c39e
Joel Aday <47754704+JoelULL@users.noreply.github.com>
2025-04-15 10:51:00 +0000

    atomic vectors

--- R/json_data_download_configuration.R
+++ R/json_data_download_configuration.R
@@ -1,6 +1,14 @@
 
 json_data_download_configuration <- function(json_file_path) {
-  params <- fromJSON(json_file_path, simplifyVector = FALSE)
+  params <- fromJSON(
+    json_file_path,
+    simplifyVector = FALSE,  
+    simplifyDataFrame = FALSE
+  )
+  params$param_codes <- lapply(params$param_codes, unlist)
+  
   do.call(download_data_filtered_v2, params)
 }
-json_data_download_configuration("json_files/parameters.json")
+
+#incode test
+#json_data_download_configuration("json_files/parameters.json")
