c156f7c66ae0077923068d7fe8d208e0c5beebb3
JoelULL <alu0100987584@ull.edu.es>
2025-04-21 13:38:28 +0100

    script to download libs added

--- develop_packages.R
+++ develop_packages.R
@@ -1,15 +1,38 @@
 # Make packages required for development visible to renv
 
-library(languageserver)
-library(lintr)
-library(styler)
-library(testthat)
-library(mockery)
-library(plumber)
-library("spanishoddata")
-library("dplyr")
-library(DBI)
-library(uuid)
-library(RPostgres)
-library(duckdb)
-library(jsonlite)
+# library(languageserver)
+# library(lintr)
+# library(styler)
+# library(testthat)
+# library(mockery)
+# library(plumber)
+# library("spanishoddata")
+# library("dplyr")
+# library(DBI)
+# library(uuid)
+# library(RPostgres)
+# library(duckdb)
+# library(jsonlite)
+
+required_packages <- c(
+  "languageserver",
+  "lintr",
+  "styler",
+  "testthat",
+  "mockery",
+  "plumber",
+  "spanishoddata",
+  "dplyr",
+  "DBI",
+  "uuid",
+  "RPostgres",
+  "duckdb",
+  "jsonlite"
+)
+
+new_packages <- required_packages[!(required_packages %in% installed.packages()[, "Package"])]
+if (length(new_packages) > 0) {
+  install.packages(new_packages)
+}
+
+invisible(lapply(required_packages, library, character.only = TRUE))
