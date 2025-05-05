aae0ed6ea20712f6bf8be2b0d355621664595f5c
Joel Aday <47754704+JoelULL@users.noreply.github.com>
2025-05-05 10:33:40 +0000

    function to get ine code per muni, dist, or lua

---
+++ experiments/get_ine_codes.R
@@ -0,0 +1,24 @@
+
+get_ine_codes <- function(option, zones, ver) {
+    ine_codes <- c()
+
+    zone_codes <- spod_get_zones(option, ver = ver)
+    zone_codes_wgs84 <- zone_codes |>
+        sf::st_simplify(dTolerance = 200) |>
+        sf::st_transform(4326)
+        
+    for (zone in zones) {
+            option_zones <- zonebuilder::zb_zone(zone)
+            insert_option_zones <- zone_codes_wgs84[option_zones, ]
+
+            #id <- unique(unlist(strsplit(as.character(insert_option_zones$id), "; ")))
+            id <- unlist(as.character(insert_option_zones$id))
+            ine_codes <- unique(c(ine_codes, id))
+    }
+
+    ine_codes
+}
+
+# canary_islands <- c("El Hierro", "La Gomera", "La Palma", "Tenerife", "Gran Canaria", "Lanzarote", "Fuerteventura", "La Graciosa")
+# result <- get_ine_codes("gau", canary_islands, 2)
+# result
